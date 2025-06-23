25f1da39c6a57c93bb7d2e03e1bd61381a3b622b
Jason Breedlove <j@jasonbreedlove.com>
2025-06-22 20:11:03 -0700

    refactor: restructure project, configure MongoDB, and simplify frontend setup
    - Removed unused assets and updated script paths in the frontend.
    - Refactored backend routes by separating controllers.
    - Added MongoDB connection utility and integrated it into the backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
+import {connectDB} from "./lib/db.js";
+import dotenv from 'dotenv'
 const app = express()
+dotenv.config()
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5006, () => {
-    console.log("Server is running on port 5006")
+const PORT = process.env.PORT || 5006
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+    connectDB()
 })
