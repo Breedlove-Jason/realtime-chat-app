b8e4cc8dbb16b79b7776b55ebdcd43f8ec8ea441
Jason Breedlove <j@jasonbreedlove.com>
2025-06-22 20:49:41 -0700

    feat: add user model, update signup controller, and configure JSON parsing middleware
    - Implemented `User` model using Mongoose.
    - Updated `signup` controller to validate password length.
    - Configured `express.json()` middleware for parsing JSON requests.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,16 @@
+import {User} from '../models/user.model.js';
+
 export const signup = (req, res) => {
-  res.send('signup route');
+  const {fullName, email, password} = req.body;
+  try{
+    if(password.length < 6) {
+      return res.status(400).send('Password must be at least 6 characters long');
+    }
+    // const user= await User
+  }catch(error) {
+    console.error('Error during signup:', error);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import authRoutes from './routes/auth.route.js'
 import {connectDB} from "./lib/db.js";
 import dotenv from 'dotenv'
+
 const app = express()
 dotenv.config()
 
+app.use(express.json())
 app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT || 5006
