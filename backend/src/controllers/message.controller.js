6feb3afe11f8216ac76006ca132b80441be5717f
Jason Breedlove <j@jasonbreedlove.com>
2025-06-24 13:14:32 -0700

    feat: extend message routes with send and fetch message functionality
    - Updated `message.route.js` to include GET endpoint for fetching messages and POST endpoint for sending messages.
    - Expanded `message.controller.js` to add `getMessages` controller with error handling.
    - Refactored `message.model.js` and improved schema formatting.
    - Minor syntax updates across routes and models for consistency.

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,33 @@
-export const getUsersForSidebar = async (req, res)=>{
+import { User } from "../models/user.model.js";
+import {Message} from "../models/message.model.js";
 
-}
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const filteredUser = await User.find({
+      _id: { $ne: loggedInUser._id },
+    }).select("-password");
+    res.status(200).json(filteredUser);
+  } catch (e) {
+    console.error("Error in getUsersForSidebar", e.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const senderId = req.user._id;
+    const messages = await Message.find({
+        $or: [
+            { senderId:senderId, receiverId: userToChatId },
+            { senderId: userToChatId, receiverId: senderId }
+        ]
+    })
+      res.status(200).json(messages);
+  } catch (e) {
+    console.error("Error in getMessages", e.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
