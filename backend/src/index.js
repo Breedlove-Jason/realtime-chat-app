82def717ed02a7d9c822b3218ee2b2a555400605
Jason Breedlove <j@jasonbreedlove.com>
2025-06-23 20:19:33 -0700

    feat: add message model, routes, and controller setup
    - Added Mongoose `messageSchema` to define message structure.
    - Introduced `/api/message` route with GET endpoint for fetching users in the sidebar.
    - Integrated `messageRoutes` into the main application.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from './routes/auth.route.js'
 import {connectDB} from "./lib/db.js";
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import messageRoutes from "./routes/message.route.js"
 
 const app = express()
 dotenv.config()
@@ -10,6 +11,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 const PORT = process.env.PORT || 5006
 app.listen(PORT, () => {
