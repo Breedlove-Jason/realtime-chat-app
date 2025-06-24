9be056dcbdba08e711c3cc94766ad2640e7adcb0
Jason Breedlove <j@jasonbreedlove.com>
2025-06-24 16:47:19 -0700

    feat: update ProfilePage, add zustand and axios integrations
    - Renamed ProfilePage title for consistency.
    - Added `zustand` for global state management and implemented `useAuthStore`.
    - Introduced Axios setup in `frontend/src/lib/axios.js` for API communication.
    - Updated dependencies in `package.json` for `zustand` and `axios`.
    - Regenerated `package-lock.json` to reflect updated dependencies.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,11 @@ import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SettingsPage from "./pages/SettingsPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx"
+import {useAuthStore} from "./store/useAuthStore.js";
 
 function App() {
+    const {authUser} = useAuthStore()
   return (
     <>
       <div>
