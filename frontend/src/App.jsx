732f2c77f7a183d284aa795a42f3351152849331
Jason Breedlove <j@jasonbreedlove.com>
2025-06-24 16:17:06 -0700

    feat: setup initial frontend pages and styling configuration
    - Added `LoginPage`, `SignUpPage`, and `Navbar` components in the `frontend/src/pages` and `frontend/src/components` directories.
    - Configured PostCSS with TailwindCSS and Autoprefixer in `postcss.config.js`.
    - Updated `index.css` to include TailwindCSS styles.
    - Installed and configured `react-router-dom`, `tailwindcss`, `daisyui`, and other dependencies for frontend development.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
+import Navbar from "./components/Navbar.jsx";
+import {Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 function App() {
-
   return (
     <>
+      <div>
+          <Navbar />
+          <Routes>
+              <Route path={"/"} element={<HomePage/>} />
+              <Route path={"/signup"} element={<SignUpPage/>} />
+              <Route path={"/login"} element={<LoginPage/>} />
+              <Route path={"/settings"} element={<SettingsPage/>} />
+              <Route path={"/profile"} element={<ProfilePage/>} />
+          </Routes>
 
+        </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
